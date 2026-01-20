acbdcd4cb2a7747f809bdf32cb46e032c5ce9980
matuiti <matsuichi39@gmail.com>
2026-01-20 11:19:53 +0900

    feat: リダイレクト機能を追加し、ルートからギャラリーページへの転送を設定

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,15 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/gallery/", // ここに確認してほしいページのパスを入れる
+        permanent: false, // 開発中の一時的な転送なのでfalse
+      },
+    ];
+  },
 };
 
 export default nextConfig;
