9257209f1e8b7b312a7f5e2a8b9e01ddd9c79b38
matuiti <matsuichi39@gmail.com>
2026-01-29 07:55:12 +0900

    feat: ホームページのレイアウトを改善し、各ページへのリンクを追加

--- next.config.mjs
+++ next.config.mjs
@@ -7,15 +7,15 @@ const nextConfig = {
     ignoreBuildErrors: true,
   },
 
-  async redirects() {
-    return [
-      {
-        source: '/',
-        destination: '/works',
-        permanent: false,
-      },
-    ];
-  },
+  // async redirects() {
+  //   return [
+  //     {
+  //       source: '/',
+  //       destination: '/works',
+  //       permanent: false,
+  //     },
+  //   ];
+  // },
 };
 
 export default nextConfig;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,12 @@ import Link from "next/link"
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 sm:items-start">
-        <Link href="./gallery/">ギャラリーページへ</Link>
+    <div className="flex flex-col mt-8 min-h-screen items-center justify-center bg-zinc-50 font-sans gap-8">
+      <h1>以下から各ページへ飛べます</h1>
+      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center gap-6">
+        <Link href="./works/">WORKSページへ</Link>
+        <Link href="./skills/">SKILLSページへ</Link>
+        <Link href="./gallery/">GALLERYページへ</Link>
       </main>
     </div>
   );
