21b0a4af59ca5eb1befec3d6e4ee431b43d4bd69
matuiti <matsuichi39@gmail.com>
2026-01-29 08:00:15 +0900

    feat: ホームページのリダイレクト設定を変更し、制作中の各ページプレビューのためのインデックスページHome2ページを追加

---
+++ src/app/home2/page.tsx
@@ -0,0 +1,14 @@
+import Link from "next/link";
+
+export default function Home2() {
+  return (
+    <div className="flex flex-col mt-8 min-h-screen items-center justify-center bg-zinc-50 font-sans gap-8">
+      <h1>以下から各ページへ飛べます</h1>
+      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center gap-6">
+        <Link href="./works/">WORKSページへ</Link>
+        <Link href="./skills/">SKILLSページへ</Link>
+        <Link href="./gallery/">GALLERYページへ</Link>
+      </main>
+    </div>
+  );
+}
