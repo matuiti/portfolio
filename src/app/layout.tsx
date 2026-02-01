c88c91eb6d2095e3aff444bf3f84967e4c303a99
matuiti <matsuichi39@gmail.com>
2026-02-01 21:11:01 +0900

    fix: TailwindCSSv4でIntelliSenseプラグインが効かないバグを修正。scss→グローバルなcssファイルを作成しTailwindに見つけてもらい理解してもらえるようにした。またCSS変数やベーススタイルもこのファイルに集約、整理。

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,21 +1,27 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import "@/styles/_base.scss";
-import { PageLayout } from "@/components/layout/PageLayout";
+import type { Metadata } from 'next';
+import { Inter, Noto_Sans_JP } from 'next/font/google';
+import "@/styles/reset.css";
+import "@/styles/globals.css";
+import { PageLayout } from '@/components/layout/PageLayout';
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
+// 英数字用: Inter
+const inter = Inter({
+  subsets: ['latin'],
+  variable: '--font-inter',
+  display: 'swap',
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+// 日本語用: Noto Sans JP
+const notoSansJp = Noto_Sans_JP({
+  subsets: ['latin'],
+  variable: '--font-noto-sans-jp',
+  display: 'swap',
+  weight: ['400', '500', '700'], // 必要な太さを指定
 });
 
 export const metadata: Metadata = {
-  title: "Portfolio",
-  description: "Showcasing my works and skills",
+  title: 'Portfolio',
+  description: 'Showcasing my works and skills',
 };
 
 export default function RootLayout({
@@ -24,10 +30,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="ja">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+    <html lang="ja" className={`${inter.variable} ${notoSansJp.variable}`}>
+      <body className="antialiased font-sans">
+        <h1 className="font-sans font-bold">TEST !!</h1>
         <PageLayout>{children}</PageLayout>
       </body>
     </html>
